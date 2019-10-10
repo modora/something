8eeb55c99cecfc6b2ad6a43e8f32caacb71d98c1
Stanley Wu <stanleywu@scrappylabs.dev>
2019-10-10 04:50:31 +0000

    removed legacy code

--- server.py
+++ server.py
@@ -11,8 +11,6 @@ logger = getLogger(__name__)
 if __name__ == "__main__":
     # Try to load in user config file; otherwise, use production config
     try:
-        # TODO: Load user config
-        _is_default_config = False  # is default config
         config_dir = Path(something.app_dirs.site_config_dir)
         config_path_py = config_dir.joinpath('config.py')
         config_path_json = config_dir.joinpath('config.json')
@@ -24,8 +22,8 @@ if __name__ == "__main__":
             app.config.from_json(config_path_json)
             logger.info(f"Using system config found at ${config_path_json}")
         else:
-            raise FileNotFoundError("No system config found")
-    except (IOError, json.decoder.JSONDecodeError) as e:
+            raise FileNotFoundError("No system config found")  # caught in IOError
+    except (IOError, json.decoder.JSONDecodeError):
         # Load in an instance of a default config
         config = {
             'production': something.config.ProdConfig,
