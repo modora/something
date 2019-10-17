be5e49227578e2e519483b1803426f0a93a901d7
Stanley Wu <stanleywu@scrappylabs.dev>
2019-10-17 19:20:33 +0000

    sanitize filename

--- something/logger.py
+++ something/logger.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from logging import getLogger
 import logging.config
 
+import werkzeug.utils
+
+
 def _is_file_handler(settings):
     """
     Checks if the handler given is a python logging handler
@@ -9,37 +12,40 @@ def _is_file_handler(settings):
     :settings:      handler settings
     """
     FILE_HANDLERS = [
-                "logging.FileHandler",
-                "logging.handlers.WatchedFileHanlder",
-                "logging.handlers.BaseRotatingHandler",
-                "logging.handlers.RotatingFileHandler",
-                "logging.handlers.TimedRotatingFileHandler",
-            ]
-    
-    if settings['class'] in FILE_HANDLERS:
+        "logging.FileHandler",
+        "logging.handlers.WatchedFileHanlder",
+        "logging.handlers.BaseRotatingHandler",
+        "logging.handlers.RotatingFileHandler",
+        "logging.handlers.TimedRotatingFileHandler",
+    ]
+
+    if settings["class"] in FILE_HANDLERS:
         return True
     return False
+
 
 def _resolve_file_handler_paths(app, config):
     # update file handlers such that all paths are absolute. Relative paths will
     # be resolved relative to the DATA_DIR config option
-    # TODO: Security - Prevent relative paths as inputs
     for handler, settings in config["handlers"].items():
-        if  _is_file_handler(settings) and not Path(settings["filename"]).is_absolute():
-            # update config to replace filename as absolute path
+        if _is_file_handler(settings) and not Path(settings["filename"]).is_absolute():
+            # sanitize filename and update config to replace filename as absolute path
+            filename = werkzeug.utils.secure_filename(settings["filename"])
             config["handlers"][handler]["filename"] = Path(
                 app.config.get("DATA_DIR")
-            ).joinpath(settings["filename"])
+            ).joinpath(filename)
+
 
 def _initialize_log_file(config):
     """
     Creates the log file if it doesn't already exist
     """
-    for settings in config['handlers'].values():
+    for settings in config["handlers"].values():
         if _is_file_handler(settings):
-            log_path = Path(settings['filename'])
+            log_path = Path(settings["filename"])
             log_path.parent.mkdir(parents=True, exist_ok=True)
             log_path.touch(exist_ok=True)
+
 
 def configure_logger(app, logger=None):
     config = app.config.get("LOG_CONFIG")
