716c43db4afaa45b6fd6720f12abcf3bf0c36822
Stanley Wu <stanleywu@scrappylabs.dev>
2019-10-27 03:16:49 +0000

    refractor api

--- something/api/v1/__init__.py
+++ something/api/v1/__init__.py
@@ -1,271 +1,184 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
 
 from . import collections
 from . import files
-from . import search
 from . import user
-from . import metadata
-from . import download
+
+from flask import jsonify, request  # delete this later, placeholder responses
 
 bp = Blueprint("v1", __name__)
 
-# Get metadata summary for all collections
-# Alias of GET /metadata/collections
-bp.add_url_rule(
-    "/collections",
-    "get_collection_summary",
-    collections.CollectionEndpoint().GET,
-    methods=["GET"],
-)
-# Create a new collection
-bp.add_url_rule(
-    "/collections",
-    "post_collections",
-    collections.CollectionEndpoint().POST,
-    methods=["POST"],
-)
-# Get metadata for the "files" collection
-bp.add_url_rule(
-    "/collections/files",
-    "get_files_collection",
-    collections.FilesCollection().GET,
-    methods=["GET"],
-)
-# Get a list of files in the archive
-bp.add_url_rule(
-    "/collections/files/items",
-    "get_filelist",
-    collections.FilesCollection().GET_items,
-    methods=["GET"],
-)
-# Upload a new file into the archive
-# Alias of POST /files
-bp.add_url_rule(
-    "/collections/files/items",
-    "post_files_collection",
-    collections.FilesCollection().POST_item,
-    methods=["POST"],
-)
-# Deletes a file from the archive
-# Alias of DELETE /files
-bp.add_url_rule(
-    "/collections/files/items",
-    "delete_file_from_files_collection",
-    collections.FilesCollection().DELETE_item,
-    methods=["DELETE"],
-)
-# Get metadata for the "collections" collection
-bp.add_url_rule(
-    "/collections/collections",
-    "get_collections_collection",
-    collections.CollectionsCollection().GET,
-    methods=["GET"],
-)
-# Get a list of collections in the archive
-bp.add_url_rule(
-    "/collections/collections/items",
-    "get_collection_list_from_collections_collection",
-    collections.CollectionsCollection().GET_items,
-    methods=["GET"],
-)
-# Create a new collection
-# Alias for POST /collections
-bp.add_url_rule(
-    "/collections/collections/items",
-    "post_collection_from_collections_collection",
-    collections.CollectionsCollection().POST_item,
-    methods=["POST"],
-)
-# Delete a collection
-# Alias of DELETE /collections/<coll_id>
-bp.add_url_rule(
-    "/collections/collections/items",
-    "delete_collection_from_collections_collection",
-    collections.CollectionsCollection().DELETE_item,
-    methods=["DELETE"],
-)
-# Get collection metadata for a given coll_id
-# Alias of GET /metadata/collections?id=<coll_id>
-bp.add_url_rule(
-    "/collections/<coll_id>",
-    "get_collection_metadata_by_id",
-    lambda coll_id: collections.BaseCollection(coll_id).GET(),
-    methods=["GET"],
-)
-# Delete the collection
-bp.add_url_rule(
-    "/collections/<coll_id>",
-    "delete_collection_by_id",
-    lambda coll_id: collections.BaseCollection(coll_id).DELETE(),
-    methods=["DELETE"],
-)
-# Get a list of items in the collection
-bp.add_url_rule(
-    "/collections/<coll_id>/items",
-    "get_items_by_collection_id",
-    lambda coll_id: collections.BaseCollection(coll_id).GET_items(),
-    methods=["GET"],
-)
-# Add a new item to the collection
-bp.add_url_rule(
-    "/collections/<coll_id>/items",
-    "post_item_by_collection_id",
-    lambda coll_id: collections.BaseCollection(coll_id).POST_item(),
-    methods=["POST"],
-)
-# Add item metadata
-# Alias for POST /metadata/items
-bp.add_url_rule(
-    "/collections/<coll_id>/items/<item_id>/metadata",
-    "post_item_metadata_by_collection_id",
-    lambda coll_id: collections.BaseCollection(coll_id).POST_item_metadata(),
-    methods=["POST"],
-)
 # Get a summary of all files in the archive
-bp.add_url_rule("/files", "get_files", files.FilesEndpoint().GET, methods=["GET"])
-# Upload a new file to the collection
-bp.add_url_rule("/files", "post_files", files.FilesEndpoint().POST, methods=["POST"])
+@bp.route("/files", method=["GET"])
+@bp.route("/metadata/files", method=["GET"])
+def get_file_summary():
+    return jsonify({"num_files": 1, "size": 2})
+
+# Get metadata summary for all collections
+@bp.route("/metadata/collections", methods=["GET"])
+@bp.route("/collections", methods=["GET"])
+def get_collection_summary():
+    return jsonify({"num_coll": 3})
+
+
+# Get metadata for the "files" collection
+@bp.route("/collections/files", methods=["POST"])
+def get_file_collection_metadata():
+    return jsonify({"coll_id": "01234567"})
+
+
+# Get metadata for the "collections" collection
+@bp.route("/collections/collections", methods=["GET"])
+def get_collection_collection_metadata():
+    return jsonify({"coll_id": "1234567", "name": "collection_name", "items": []})
+
+
+# Get a list of files in the archive
+@bp.route("/collections/files/items", methods=["GET"])
+def get_file_list():
+    return jsonify({"items": ["item1", "item2"]})
+
+
+# Get the list of collections in the archive
+@bp.route("/collections/collections/items", methods=["GET"])
+def get_collection_list():
+    return jsonify({"items": ["item1", "item2"]})
+
+
+# Upload a new file into the archive
+@bp.route("/files", methods=["POST"])
+@bp.route("/collections/files/items", methods=["POST"])
+def upload_file():
+    return jsonify({"file_id": "01234567"})
+
+
+# Deletes a file from the archive
+@bp.route("/files/<file_id>", methods=["DELETE"])
+@bp.route("/collections/files/items/<file_id>", methods=["DELETE"])
+def delete_file(file_id):
+    return jsonify({"return_code": 0})
+
+
+# Create a new collection
+@bp.route("/collections", methods=["POST"])
+@bp.route("/collections/collections/items", methods=["POST"])
+def create_new_collection():
+    return jsonify({"coll_id": "01234567"})
+
+
+# Delete a collection
+@bp.route("/collections/collections/items/<coll_id>", methods=["DELETE"])
+@bp.route("/collections/<coll_id>", methods=["DELETE"])
+def delete_collection(coll_id):
+    return jsonify({"return_code": 0})
+
+# Get a list of items in the collection
+@bp.route("/collections/<coll_id>/items", methods=["GET"])
+def get_collection_items(coll_id):
+    return jsonify({"items": ["item1", "item2"]})
+
+# Add a new item to the collection
+@bp.route("/collections/<coll_id>/items", methods=["POST"])
+def add_collection_item(coll_id):
+    return jsonify({"item_id": "01234567"})
+
+# Remove an item from the collection
+@bp.route("/collections/<coll_id>/items/<item_id>", methods=["DELETE"])
+def delete_collection_item(coll_id, item_id):
+    return jsonify({"coll_id": coll_id,"item_id": item_id, "return_code": 0})
+
 # Get file metadata
-# Alias of /metadata/files?<file_id>
-bp.add_url_rule(
-    "/files/<file_id>",
-    "get_file_by_id",
-    lambda file_id: files.BaseFilesEndpoint(file_id).GET(),
-    methods=["GET"],
-)
-# Removes the file from the archive
-bp.add_url_rule(
-    "/files/<file_id>",
-    "delete_file_by_id",
-    lambda file_id: files.BaseFilesEndpoint(file_id).DELETE(),
-    methods=["DELETE"],
-)
-# Get file metadata
-# Alias of GET /metadata/files?file_id=<file_id>
-bp.add_url_rule(
-    "/files/<file_id>/metadata",
-    "get_metadata_by_file_id",
-    lambda file_id: files.BaseFilesEndpoint(file_id).GET_metadata(),
-    methods=["GET"],
-)
+@bp.route("/files/<file_id>")
+@bp.route("/metadata/files/file_id")
+def get_file_metadata(file_id):
+    return jsonify({"item_of": ["coll_id1"]})
+
+
 # Set file metadata
-# Alias of POST /metadata/files
-bp.add_url_rule(
-    "/files/<file_id>/metadata",
-    "post_metadata_by_file_id",
-    lambda file_id: files.BaseFilesEndpoint(file_id).POST_metadata(),
-    methods=["POST"],
-)
+@bp.route("/files/<file_id>/metadata", methods=["POST"])
+@bp.route("/metadata/files/<file_id>", methods=["POST"])
+def set_file_metadata(file_id):
+    return jsonify({"file_id": file_id, "key1": "value1", "key1.key3": "value3"})
+
+
 # Modify file metadata
-# Alias of PUT /metadata/files
-bp.add_url_rule(
-    "/files/<file_id>/metadata",
-    "put_metadata_by_file_id",
-    lambda file_id: files.BaseFilesEndpoint(file_id).PUT_metadata(),
-    methods=["PUT"],
-)
+@bp.route("/files/<file_id>/metadata", methods=["PUT"])
+@bp.route("/metadata/files/<file_id>", methods=["PUT"])
+def update_file_metadata(file_id):
+    return jsonify({"coll_id": file_id, "key": "value"})
+
+
 # Delete file metadata
-# Alias of DELETE /metadata/files?file_id=<file_id>
-bp.add_url_rule(
-    "/files/<file_id>/metadata",
-    "delete_metadata_by_file_id",
-    lambda file_id: files.BaseFilesEndpoint(file_id).DELETE_metadata(),
-    methods=["DELETE"],
-)
-# Search archive
-bp.add_url_rule("/search", "search_archive", search.Search.GET, methods=["GET"])
-# Authenticate user credentials
-bp.add_url_rule(
-    "/user/auth", "authenticate_user", user.User.POST_authenticate, methods=["POST"]
-)
+@bp.route("/files/<file_id>/metadata", methods=["DELETE"])
+@bp.route("/metadata/files/<file_id>", methods=["DELETE"])
+def delete_file_metadata(file_id):
+    return jsonify({"return_code": 0, "deleted_keys": ["key1"]})
+
+
 # Get collection metadata
-bp.add_url_rule(
-    "/metadata/collections",
-    "get_collection_metadata",
-    metadata.CollectionMetadata("coll_id").GET,
-    methods=["GET"],
-)
-# Add collection metadata
-bp.add_url_rule(
-    "/metadata/collections",
-    "post_collection_metadata",
-    metadata.CollectionMetadata("coll_id").POST,
-    methods=["POST"],
-)
+@bp.route("/collections/<coll_id>", methods=["GET"])
+@bp.route("/metadata/collections/<coll_id>", methods=["GET"])
+def get_collection_metadata(coll_id):
+    return jsonify({"coll_id": "123456", "name": "collection_name", "items": []})
+
+
+# Set collection metadata
+@bp.route("/collections/<coll_id>/metadata", methods=["POST"])
+@bp.route("/metadata/collections/<coll_id>", methods=["POST"])
+def set_collection_metadata(coll_id):
+    return jsonify({"coll_id": coll_id, "key": "value"})
+
+
 # Modify collection metadata
-bp.add_url_rule(
-    "/metadata/collections",
-    "put_collection_metadata",
-    metadata.CollectionMetadata("coll_id").PUT,
-    methods=["PUT"],
-)
-# Remove collection metadata
-bp.add_url_rule(
-    "/metadata/collections",
-    "delete_collection_metadata",
-    metadata.CollectionMetadata("coll_id").DELETE,
-    methods=["DELETE"],
-)
+@bp.route("/collections/<coll_id>/metadata", methods=["PUT"])
+@bp.route("/metadata/collections/<coll_id>", methods=["PUT"])
+def update_collection_metadata(coll_id):
+    return jsonify({"coll_id": coll_id, "key": "value"})
+
+
+# Delete collection metadata
+@bp.route("/collections/<coll_id>/metadata", methods=["DELETE"])
+@bp.route("/metadata/collections/<coll_id>", methods=["DELETE"])
+def delete_collection_metadata(coll_id):
+    return jsonify({"coll_id": coll_id, "return_code": 0})
+
 # Get item metadata
-bp.add_url_rule(
-    "/metadata/items",
-    "get_item_metadata",
-    metadata.ItemMetadata("coll_id", "item_id").GET,
-    methods=["GET"],
-)
+@bp.route("/collections/<coll_id>/items/<item_id>/metadata", methods=["POST"])
+def get_item_metadata(coll_id, item_id):
+    return jsonify({"coll_id": coll_id, "item_id": item_id, "key1": "value1"})
+
 # Add item metadata
-bp.add_url_rule(
-    "/metadata/items",
-    "post_item_metadata",
-    metadata.ItemMetadata("coll_id", "item_id").POST,
-    methods=["POST"],
-)
-# Modify item metadata
-bp.add_url_rule(
-    "/metadata/items",
-    "put_item_metadata",
-    metadata.ItemMetadata("coll_id", "item_id").PUT,
-    methods=["PUT"],
-)
+@bp.route("/collections/<coll_id>/items/<item_id>/metadata", methods=["POST"])
+def set_item_metadata(coll_id, item_id):
+    return jsonify({"coll_id": coll_id, "item_id": item_id, "key1": "value1"})
+
+# Update item metadata
+@bp.route("/collections/<coll_id>/items/<item_id>/metadata", methods=["POST"])
+def update_item_metadata(coll_id, item_id):
+    return jsonify({"coll_id": coll_id, "item_id": item_id, "key": "new_value"})
+
 # Remove item metadata
-bp.add_url_rule(
-    "/metadata/items",
-    "delete_item_metadata",
-    metadata.ItemMetadata("coll_id", "item_id").DELETE,
-    methods=["DELETE"],
-)
-# Get file metadata
-bp.add_url_rule(
-    "/metadata/files",
-    "get_file_metadata",
-    metadata.FileMetadata("file_id").GET,
-    methods=["GET"],
-)
-# Add file metadata
-bp.add_url_rule(
-    "/metadata/files",
-    "post_file_metadata",
-    metadata.FileMetadata("file_id").POST,
-    methods=["POST"],
-)
-# Modify file metadata
-bp.add_url_rule(
-    "/metadata/files",
-    "put_file_metadata",
-    metadata.FileMetadata("file_id").PUT,
-    methods=["PUT"],
-)
-# Remove file metadata
-bp.add_url_rule(
-    "/metadata/files",
-    "delete_file_metadata",
-    metadata.FileMetadata("file_id").DELETE,
-    methods=["DELETE"],
-)
+@bp.route("/collections/<coll_id>/items/<item_id>/metadata", methods=["POST"])
+def delete_item_metadata(coll_id, item_id):
+    return jsonify({"coll_id": coll_id, "item_id": item_id, "return_code": 0})
+
+
+# Search archive
+@bp.route("/search", methods=["GET"])
+def search_archive():
+    return jsonify({"results": [{"type": "file", "id": "01234567"}]})
+
+
+# Authenticate user credentials
+@bp.route("/user/auth", methods=["POST"])
+def authenticate():
+    return "JWT"
+
+
+
 # Download a single file or collection as a zip
-bp.add_url_rule(
-    "/download",
-    "download_file",
-    lambda: download.Download.from_request().download(),
-    methods=["GET"],
-)
+@bp.route("/download")
+def download(file_id=None, coll_id=None):
+    file_id = request.args["file_id"]
+    coll_id = request.args["coll_id"]
+    return "file or collection"
--- something/api/v1/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    @staticmethod
-    def POST_authenticate():
-        """
-        Authenticate user login credentials
-        :return:    JWT
-        """
-        return "JWT"
